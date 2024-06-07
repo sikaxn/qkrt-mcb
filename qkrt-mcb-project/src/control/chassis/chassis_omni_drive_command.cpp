988ed3d8d50ea63ce48c5d3124fa8b11229585a5
hamzaiqbal10 <20hi4@queensu.ca>
2024-06-06 20:29:53 -0400

    sentry can move a distance and then stop permenantly

--- qkrt-mcb-project/src/control/chassis/chassis_omni_drive_command.cpp
+++ qkrt-mcb-project/src/control/chassis/chassis_omni_drive_command.cpp
@@ -17,12 +17,49 @@
  * along with qkrt-mcb.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+// #include "chassis_omni_drive_command.hpp"
+
+// #include "tap/algorithms/math_user_utils.hpp"
+
+// #include "control/control_operator_interface.hpp"
+
+// #include "chassis_subsystem.hpp"
+
+// #include <thread>  // Include the thread library for delays
+// #include <chrono>  // Include the chrono library for time duration
+
+// using tap::algorithms::limitVal;
+
+// namespace control::chassis
+// {
+// ChassisOmniDriveCommand::ChassisOmniDriveCommand(
+//     ChassisSubsystem &chassis,
+//     ControlOperatorInterface &operatorInterface)
+//     : chassis(chassis),
+//       operatorInterface(operatorInterface)
+// {
+//     addSubsystemRequirement(&chassis);
+// }
+
+// void ChassisOmniDriveCommand::execute()
+// {
+//     auto scale = [](float raw) -> float
+//     { return limitVal(raw, -1.0f, 1.0f) * MAX_CHASSIS_SPEED_MPS; };
+//     chassis.setVelocityOmniDrive(scale(1), scale(1), scale(1), scale(1));
+//     modm::delay_ms(50000);
+//     chassis.setVelocityOmniDrive(scale(-1), scale(-1), scale(-1), scale(-1));
+//     modm::delay_ms(50000);
+// }
+
+// void ChassisOmniDriveCommand::end(bool) { chassis.setVelocityOmniDrive(.0f, .0f, .0f, .0f); }
+
+// };  // namespace control::chassis
+
 #include "chassis_omni_drive_command.hpp"
-
+#include <chrono>  // Include the chrono library for time duration
+#include <thread>  // Include the thread library for delays
 #include "tap/algorithms/math_user_utils.hpp"
-
 #include "control/control_operator_interface.hpp"
-
 #include "chassis_subsystem.hpp"
 
 using tap::algorithms::limitVal;
@@ -38,20 +75,27 @@ ChassisOmniDriveCommand::ChassisOmniDriveCommand(
     addSubsystemRequirement(&chassis);
 }
 
+bool flag = true;
+
+
+//NOTE: calculate the actual distance it covers and the speed the robot moves at to get the time in seconds or miliseconds.
+//Use that calculated time as delay to make the robot cover that exact distance and stop
+
+
 void ChassisOmniDriveCommand::execute()
 {
-    operatorInterface.pollInputDevices();
+    if (flag)
+    {
+        chassis.setVelocityOmniDrive(1, 1, 1, 1);
+        modm::delay_ms(50000);
 
-    auto scale = [](float raw) -> float {
-        return limitVal(raw, -1.0f, 1.0f) * MAX_CHASSIS_SPEED_MPS;
-    };
-
-    chassis.setVelocityOmniDrive(
-        scale(operatorInterface.getChassisOmniLeftFrontInput()),
-        scale(operatorInterface.getChassisOmniLeftBackInput()),
-        scale(operatorInterface.getChassisOmniRightFrontInput()),
-        scale(operatorInterface.getChassisOmniRightBackInput())
-    );
+        flag = false;
+    }
+    else
+    {
+        chassis.setVelocityOmniDrive(0, 0, 0, 0);
+        modm::delay_ms(50000);
+    }
 }
 
 void ChassisOmniDriveCommand::end(bool) { chassis.setVelocityOmniDrive(.0f, .0f, .0f, .0f); }
